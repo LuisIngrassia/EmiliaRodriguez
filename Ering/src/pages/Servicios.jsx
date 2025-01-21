7605533d3308472cd07cda8fb181f266af83de0d
LuisIngrassia <ingrassialuispedro@gmail.com>
2025-01-21 09:42:07 -0300

    contact form

--- Ering/src/pages/Servicios.jsx
+++ Ering/src/pages/Servicios.jsx
@@ -13,6 +13,23 @@ import {
 } from "lucide-react";
 
 const Servicios = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    consultationType: "",
+    preferedDate: "",
+    message: "",
+  });
+  const [status, setStatus] = useState(""); // Para mostrar el estado del envío
+
+  // Manejar los cambios en los inputs
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
   // Enviar el formulario
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -268,14 +285,14 @@ const Servicios = () => {
               </div>
               <div>
                 <label
-                  htmlFor="consultation-type"
+                  htmlFor="consultationType"
                   className="block text-sm font-medium text-gray-700 mb-1"
                 >
                   Tipo de Consulta
                 </label>
                 <select
-                  id="consultation-type"
-                  name="consultation-type"
+                  id="consultationType"
+                  name="consultationType"
                   required
                   className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
                 >
@@ -290,15 +307,15 @@ const Servicios = () => {
               </div>
               <div>
                 <label
-                  htmlFor="preferred-date"
+                  htmlFor="preferredDate"
                   className="block text-sm font-medium text-gray-700 mb-1"
                 >
                   Fecha Preferida
                 </label>
                 <input
                   type="date"
-                  id="preferred-date"
-                  name="preferred-date"
+                  id="preferredDate"
+                  name="preferredDate"
                   required
                   className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
                 />
